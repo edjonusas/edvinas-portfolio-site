cbd15f3d0d38f840cfeec8d7d4d3b4a32e277bb8
Edvinas Jonusas <ed.jonusas@gmail.com>
2021-02-10 12:50:38 +0200

    added netlify identety widget

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,10 @@ import Home from '~/components/Home/Home.vue'
 
 export default {
   components: { Home },
-  async fetch() {
-    await this.$store.dispatch('getHomePage')
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    }
   },
 }
 </script>
