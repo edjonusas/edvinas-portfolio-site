6463e9fa613f6864a335e33369a08259c900c0cd
Edvinas Jonusas <ed.jonusas@gmail.com>
2021-02-11 20:51:00 +0200

    added portfolio cards info to cms

---
+++ components/Portfolio/PortfolioCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :style="{ backgroundImage: portfolio.portfolioImage }">
+    <div>{{ portfolio.title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    portfolio: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ components/Portfolio/PortfolioList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="portfolio in portfolioList" :key="portfolio.slug">
+      <PortfolioCard :portfolio="portfolio" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    portfolioList: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
