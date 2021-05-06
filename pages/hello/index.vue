740b9a8009c00c6092dce014277aab923503e4f2
Edvinas Jonusas <ed.jonusas@gmail.com>
2021-05-06 13:59:38 +0300

    page titles and descriptions

---
+++ pages/hello/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <Home :home-page="homePage" />
+  </section>
+</template>
+
+<script>
+import Home from '~/components/Home/Home.vue'
+
+export default {
+  components: { Home },
+  async asyncData({ $content }) {
+    const homePage = await $content('home').fetch()
+    return { homePage }
+  },
+  head() {
+    return {
+      title: 'Welcome to Developed By Edwin Home page',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Developed By Edwin home page',
+        },
+      ],
+    }
+  },
+}
+</script>
